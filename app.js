8620db6b4cdcf638f1d2cca788efc22da99b7e73
avenmia <mavend2@gmail.com>
2022-10-22 19:16:45 -1000

    removing multer and extra keys

--- app.js
+++ app.js
@@ -15,9 +15,6 @@ const path = require('path');
 const mongoose = require('mongoose');
 const passport = require('passport');
 const sass = require('node-sass-middleware');
-const multer = require('multer');
-
-const upload = multer({ dest: path.join(__dirname, 'uploads') });
 
 /**
  * Load environment variables from .env file, where API keys and passwords are configured.
