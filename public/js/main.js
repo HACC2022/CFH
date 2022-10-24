e9441e8898adf9aa9804f06a2fb7e59cc62aad62
avenmia <mavend2@gmail.com>
2022-10-23 19:17:23 -1000

    Adding event handler on submit button

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,7 @@
 /* eslint-env jquery, browser */
 $(document).ready(() => {
   // Place JavaScript code here...
+  $('#submit-btn').on('click', async () => {
+    console.log('Button clicked!');
+  });
 });
