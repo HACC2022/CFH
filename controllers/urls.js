ebbb371bd3a27f895bd0a5560f0a8b8f83b123f9
Boobeh123 <boobeh321@gmail.com>
2022-10-25 16:22:24 -1000

    Render User & Url models

--- controllers/urls.js
+++ controllers/urls.js
@@ -2,8 +2,21 @@
  * GET /urls
  * URLs page.
  */
- exports.index = (req, res) => {
+const { request } = require('gaxios');
+const Url = require('../models/Url')
+const User = require('../models/User')
+
+exports.index = async (req, res) => {
+  try {
+    const links = await Url.find()
+    const users = await User.find()
     res.render('urls', {
-      title: 'URLs'
-    });
-  };
+      title: 'URLs',
+      urlInfo:links,
+      currentUser:req.user.id,
+      userInfo:users
+    })
+  } catch(error) {
+    console.error(error)
+  }
+};
