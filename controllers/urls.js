07e0a12527517748d0c7230a73252e9a8e8ca9d7
Boobeh123 <boobeh321@gmail.com>
2022-10-27 16:58:41 -1000

    Add sortDescending, sortAscending, and editUrl methods

--- controllers/urls.js
+++ controllers/urls.js
@@ -30,5 +30,77 @@ module.exports = {
     } catch(error) {
         console.error(error)
     }
+  },
+  sortDescending: async (req, res) => {
+    try {
+      const links = await Url.find().sort({date:-1})
+      const users = await User.find()
+      res.render('urls', {
+        title: 'URLs',
+        urlInfo:links,
+        currentUser:req.user.id,
+        userInfo:users,
+        visits: links.clickCounter,
+      })
+    } catch(error) {
+      console.error(error)
+    }
+  },
+  sortAscending: async (req, res) => {
+    try {
+      const links = await Url.find().sort({date:1})
+      const users = await User.find()
+      res.render('urls', {
+        title: 'URLs',
+        urlInfo:links,
+        currentUser:req.user.id,
+        userInfo:users,
+        visits: links.clickCounter,
+      })
+    } catch(error) {
+      console.error(error)
+    }
+  },
+  editUrl: async (req, res) => {
+    try {
+      let links = await Url.findById(req.params.id)
+      let users = await User.find({email:links.user})
+
+      if (!users) {
+        res.redirect('/')
+      }
+      
+      if (users[0].email != req.user.email) {
+        res.redirect('/urls')
+      } else {
+        res.render('edit', {
+          title: 'Edit Url',
+          users,
+          links})
+      }
+    } catch(error) {
+      console.error(error)
+    }
+  },
+  updateUrl: async (req, res) => {
+    // TODO: Edit functionality
+    try {
+      console.log('if u see this it works')
+      // if (!users) {
+      //   res.redirect('/')
+      // }
+      
+      // if (users[0].email != req.user.email) {
+      //   res.redirect('/urls')
+      // } else {
+      //   links = await Url.findOneAndUpdate({_id:req.params.id}, req.body, {
+      //     new: true,
+      //     runValidators: true
+      //   })
+      //   res.redirect('/urls')
+      // }
+    } catch(error) {
+      console.error(error)
+    }
   }
 };
