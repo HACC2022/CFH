a3e642da2a01dc09e077474801414ce8dd1eb4ca
Boobeh123 <boobeh321@gmail.com>
2022-10-28 16:02:19 -1000

    Edit updateUrl function. Edit ID within querySelectorAll

--- public/js/urlBtns.js
+++ public/js/urlBtns.js
@@ -1,15 +1,33 @@
-const editBtn = document.querySelectorAll('#editUrlBtn')
+const editBtn = document.querySelectorAll('#updateEditBtn')
 const deleteBtn = document.querySelectorAll('#delUrlBtn')
 
 Array.from(editBtn).forEach((element) => {
-    element.addEventListener('click', editUrl)
+    element.addEventListener('click', updateUrl)
 })
 Array.from(deleteBtn).forEach((element) => {
     element.addEventListener('click', deleteUrl)
 })
 
-async function editUrl() {
-    console.log('olleh');
+async function updateUrl() {
+    const longUrl = this.parentNode.childNodes[1].value
+    const slug = this.parentNode.childNodes[4].value
+    const id = this.parentNode.dataset.id
+    try {
+        const response = await fetch(`/urls/${id}`, {
+            method: 'put',
+            headers: {'Content-type': 'application/json'},
+            body: JSON.stringify({
+                'idFromJSFile': id,
+                'urlFromJSFile': longUrl,
+                'slugFromJSFile': slug,
+            })
+        })
+        const data = await response.json()
+        console.log(data)
+        location.replace('/urls')
+    } catch(error) {
+        console.error(error)
+    }
 }
 async function deleteUrl() {
     const slug = this.parentNode.parentNode.childNodes[0].innerText
